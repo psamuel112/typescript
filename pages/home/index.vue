4ac64eb23e1d01e50370f5958b7a1bad5d087b58
psamuel112 <81156000+psamuel112@users.noreply.github.com>
2024-10-23 05:32:06 +0100

    test app

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -4,22 +4,21 @@
       <div class="content">
         <h1 class="display-3 fw-bold mb-4">Welcome to My Project</h1>
         <p class="lead mb-5">
-            This project is built with TypeScript, Bootstrap, Vuetify, and Pinia, offering a smooth experience for managing users and navigating through user details. Dive into exploring how these technologies are integrated to create a seamless interface for handling user data.
+          This project is built with TypeScript, Bootstrap, Vuetify, and Pinia, offering a smooth experience for
+          managing users and navigating through user details. Dive into exploring how these technologies are integrated
+          to create a seamless interface for handling user data.
         </p>
         <v-btn class="px-8 py-2" color="primary" flat @click="exploreUsers">Explore Users</v-btn>
       </div>
-    
     </div>
- <p class="text-center">Yekinni Damilola</p>
   </div>
+</template>
 
-  </template>
-  
 <script lang="ts" setup>
 const router = useRouter();
 const exploreUsers = () => {
-    console.log("check")
-    router.push('/users')
+  console.log("check")
+  router.push('/users')
 }
 </script>
 <style scoped>
